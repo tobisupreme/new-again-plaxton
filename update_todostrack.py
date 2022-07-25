722a0f8985fe10339c08ca5dd6e7227fbbaeb977
Tobi Balogun <tobi.balo@gmail.com>
2022-07-25 20:01:39 +0100

    practice fetching results

--- update_todostrack.py
+++ update_todostrack.py
@@ -1,3 +1,4 @@
+from unittest import result
 import psycopg2
 
 # define database to connect to
@@ -10,13 +11,34 @@ conn = psycopg2.connect(dbname)
 cur = conn.cursor()
 
 # add rows to todos_track table
-cur.execute('INSERT INTO todos_track (id, completed) VALUES (%s, %s);', (1, False))
+# cur.execute('INSERT INTO todos_track (id, completed) VALUES (%s, %s);', (1, False))
 
-data = {
-  'id': 2,
-  'completed': True
-}
-cur.execute('INSERT INTO todos_track (id, completed) VALUES (%(id)s, %(completed)s);', data)
+# query2 = 'INSERT INTO todos_track (id, completed) VALUES (%(id)s, %(completed)s);'
+# data2 = {
+#   'id': 2,
+#   'completed': True
+# }
+# cur.execute(query2, data2)
+
+# query3 = 'INSERT INTO todos_track (id, completed) VALUES (%(id)s, %(completed)s);'
+# data3 = {
+#   'id': 3,
+#   'completed': False
+# }
+# cur.execute(query3, data3)
+
+# fetch results
+cur.execute('SELECT * FROM todos_track;')
+
+result1 = cur.fetchone()
+print('fetchone', result1)
+
+cur.execute('SELECT * FROM todos_track;')
+result2 = cur.fetchone()
+print('fetchone', result2)
+
+result3 = cur.fetchall()
+print('fetchall', result3)
 
 # commit changes
 conn.commit()
