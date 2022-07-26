c39a7f6e7497bd9fdd8136a41664e339cc4b3ecd
Tobi Balogun <tobi.balo@gmail.com>
2022-07-26 14:21:37 +0100

    Use SQLAlchemy

--- flask_sqlalchemyorm/start.py
+++ flask_sqlalchemyorm/start.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tobisupreme:tobi@localhost:5432/example'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -9,4 +12,4 @@ def index():
 
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0",port=5003)
+    app.run(host="0.0.0.0", port=5003)
