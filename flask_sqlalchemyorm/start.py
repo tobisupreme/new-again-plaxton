483831b4c6b8d1ef0db24b428423f13e01495c6b
Tobi Balogun <tobi.balo@gmail.com>
2022-07-26 16:23:12 +0100

    Use result from db query in hello app

--- flask_sqlalchemyorm/start.py
+++ flask_sqlalchemyorm/start.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tobisupreme:tobi@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -11,14 +12,19 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
+    def __repr__(self):
+        return f'[Person ID: {self.id}, name: {self.name}]'
+
 
 db.create_all()
 
 
 @app.route('/')
 def index():
-    return 'hello world! I am here'
+    person = Person.query.all()
+    return 'hello world! ' + person[0].name + ' is here o!'
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host="0.0.0.0", port=5003)
