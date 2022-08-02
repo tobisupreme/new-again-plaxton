e3f54163a609fb6636ad66f28af75c078ed513ae
Tobi Balogun <tobi.balo@gmail.com>
2022-08-02 22:33:00 +0100

    Update database table using flask migrate

--- app/app.py
+++ app/app.py
@@ -14,6 +14,7 @@ class Tasks(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'[Task ID: {self.id}, Task: {self.name}]'
