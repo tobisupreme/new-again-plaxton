00fb737579bf6e6864d9e83746b4ef7150ee38ca
Tobi Balogun <tobi.balo@gmail.com>
2022-08-02 22:58:55 +0100

    Add entries to database and update views of dummy tasks app accordingly

--- taskapp/app.py
+++ taskapp/app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tobisupreme:tobi@localhost:5432/dummytasksapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+
+class Tasks(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'[Task ID = {self.id} | Task = {self.task}]'
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'task': 'Strike a pose'
-    }, {
-        'task': 'Bend the knee'
-    }, {
-        'task': 'parse the html'
-    }])
+    return render_template('index.html', data=Tasks.query.all())
 
 
 if __name__ == '__main__':
